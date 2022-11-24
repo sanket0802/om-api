c2ffc679a0094f091268bbf1cb9862c23fc4a5c5
sanket0802 <93255738+sanket0802@users.noreply.github.com>
2022-11-24 14:48:30 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,6 @@
     sudo apt-get update -y 
     wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
     sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-    sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
     wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add - 
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
     sudo apt-get update
