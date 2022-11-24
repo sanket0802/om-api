98d321c17832807bc52ccfedfaa2cf3824d93a0b
sanket0802 <93255738+sanket0802@users.noreply.github.com>
2022-11-24 16:05:58 +0530

    Update index.js

--- omni-meet-api/index.js
+++ omni-meet-api/index.js
@@ -19,7 +19,7 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-    res.send('bye World!');
+    res.send('bye World! get lost ');
   });
 
 const corsOptions = {
